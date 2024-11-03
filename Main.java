a0b66161d8422ef1ebe7b9fcc493078995bfcefc
Talgat <tomyrkanov@gmail.com>
2024-11-03 16:00:01 +0500

    The Main class where executed the main work of program and gitignore file with IDE files which immutable

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
-class Main{
-    public static void main(String[] args){
-        System.out.println("Jambo hui");
+public class Main {
+    public static void main(String[] args) {
+
+        CourierService courierService = new CourierService();
+        RealTrackingService realTrackingService = new RealTrackingService();
+        TrackingService trackingServiceProxy = new TrackingServiceProxy(realTrackingService);
+        ETACalculator etaCalculator = new BasicETACalculator();
+
+        Notification basicNotification = new BasicNotification();
+        Notification smsNotification = new SMSNotificationDecorator(basicNotification);
+        Notification pushNotification = new PushNotificationDecorator(smsNotification);
+
+        DeliveryFacade deliveryFacade = new DeliveryFacade(courierService, trackingServiceProxy, pushNotification, etaCalculator);
+
+        deliveryFacade.startDelivery();
+
+        Courier courier = courierService.assignCourier();  // Назначаем того же курьера для завершения
+        deliveryFacade.completeDelivery(courier);
     }
 }
